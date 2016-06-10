61399a4cf88959eaff1d35b8f5c9307d1c607d35
Galen <galen.scovell@gmail.com>
2016-06-09 19:13:31 -0700

    Stemming and Bigrams, LDA multicore

--- data_munging.py
+++ data_munging.py
@@ -8,27 +8,45 @@ Basic data munging to produce cleaned data sets for other scripts.
 
 
 import string
+import nltk
 from nltk.corpus import stopwords
+from nltk.collocations import *
+from nltk.stem.porter import PorterStemmer
 
 
 punc = string.punctuation + '\n\r\t'
 stop = stopwords.words('english')
 
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+p_stemmer = PorterStemmer()
+
+
+def stem(words):
+    stemmed_words = [p_stemmer.stem(word) for word in words]
+    return stemmed_words
+
+
+def bigram(words, n):
+    finder = BigramCollocationFinder.from_words(words)
+    raw_bigrams = finder.nbest(bigram_measures.pmi, n)
+    bigrams = ['_'.join(bigram) for bigram in raw_bigrams]
+    return bigrams
+
 
 def clean_data(file_name):
     cleaned = []
-
     with open(file_name, 'r') as f:
         for line in f:
-            cleaned_words = []
+            result = []
             for p in punc:
                 line = line.replace(p, '')
             words = line.lower().rstrip().split(' ')
             for word in words:
                 if word not in stop and not word.isdigit():
-                    cleaned_words.append(word)
-            cleaned.append(' '.join(cleaned_words))
-
+                    result.append(word)
+            stemmed = stem(result)
+            result = bigram(stemmed, 10)
+            cleaned.append(' '.join(result))
     return cleaned
 
 
--- lda_topic_modeling.py
+++ lda_topic_modeling.py
@@ -30,11 +30,12 @@ def create_and_train_model(term_dict, corpus):
     # Setup logging messages
     logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', level=logging.INFO)
 
-    model = gensim.models.LdaModel(
+    model = gensim.models.LdaMulticore(
         corpus=corpus,       # Corpus
         num_topics=4,        # Number of topics to generate
         id2word=term_dict,   # Map for id (int) to word (string)
-        passes=20            # Iterations over corpus, higher is more accurate but slower
+        passes=20,           # Iterations over corpus, higher is more accurate but slower
+        workers=2            # Parallelized workers
     )
     model.save('lda_model')
 
